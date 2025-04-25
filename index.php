c643a5e9dbddd435db693363a48247b708726a03
rawdasherif0 <rawdasherif012@gmail.com>
2025-04-25 22:15:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 <html>
 <head>
    
-    <title> Login</title>
+    <title>Login</title>
     <link rel="stylesheet" href="style/main.css">
-</head>
+    </head>
 <body>
   <form action="">
     <div class="main">     
-      <h1>Login</h1>
+      <h2>Login</h2>
       <label for=""> Username</label>
-      <Input type= "text" name="username" id="username" placeholder="Enter your Username" required><br>
+      <input type= "text" name="username" id="username" placeholder="Enter your Username" required><br>
       <label for=""> Password</label>
-      <Input type= "password" name="password" id="password" placeholder="Enter your Password" required><br>
-      <Input type= "submit" name="submit" id="submit" value="Login"><br>
+      <input type= "password" name="password" id="password" placeholder="Enter your Password" required><br>
+      <input type= "submit" name="submit" id="submit" value="Login"><br>
       <h3>Or</h3>
       <b class="login-link"> Don't have an account? <a href="register.php">Create an account</a></b>
     </div>  
