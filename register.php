60c1c51cffed5b450f1f3026af8eacd1fe15146b
rawdasherif0 <rawdasherif012@gmail.com>
2025-04-25 22:15:54 +0300

    Update register.php

--- register.php
+++ register.php
@@ -4,24 +4,24 @@
    
     <title>Register</title>
     <link rel="stylesheet" href="style/main.css">
-</head>
+    </head>
 <body>
   <form action="">
     <div class = "main"> 
-      <h1>Register</h1>
+      <h2>Register</h2>
       <div class="mb-3">
         <label for=""> Username</label>
-        <Input type= "text" name="username" id="username" placeholder="Enter your Username" required><br>
+        <input type= "text" name="username" id="username" placeholder="Enter your Username" required><br>
       </div>
       <div class="mb-3">
         <label for=""> Email</label>
-        <Input type= "text" name="email" id="email" placeholder="Enter your Email" required><br>
+        <input type= "text" name="email" id="email" placeholder="Enter your Email" required><br>
       </div>
       <div class="mb-3">
         <label for=""> Password</label>
-        <Input type= "password" name="password" id="password" placeholder="Enter your Password" required><br>
+        <input type= "password" name="password" id="password" placeholder="Enter your Password" required><br>
       </div>
-      <Input type= "submit" name="submit" id="submit" value="Register Account"><br>
+      <input type= "submit" name="submit" id="submit" value="Register Account"><br>
       <h3>Or</h3>
       <p class="login-link">Already have an account? <a href="index.php">Login</a></p>
     </div>
