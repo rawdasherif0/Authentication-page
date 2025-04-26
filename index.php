885955ccba62fd53a52723acd3962f053ee8e930
rawdasherif0 <rawdasherif012@gmail.com>
2025-04-26 04:52:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 <head>
    
     <title>Login</title>
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="/Mazaj/style/main.css">
     </head>
 <body>
-  <form action="">
+  <form action="index_post.php" method = "POST">
     <div class="main">     
       <h2>Login</h2>
+      <?php if (isset($user_error)) echo $user_error; ?>
       <label for=""> Username</label>
       <input type= "text" name="username" id="username" placeholder="Enter your Username" required><br>
+      <?php if (isset($password_error)) echo $password_error; ?>
       <label for=""> Password</label>
       <input type= "password" name="password" id="password" placeholder="Enter your Password" required><br>
       <input type= "submit" name="submit" id="submit" value="Login"><br>
