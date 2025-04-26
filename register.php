1e12c436605a9266bc54601d5dff20e4df2196ea
rawdasherif0 <rawdasherif012@gmail.com>
2025-04-26 04:51:44 +0300

    Update register.php

--- register.php
+++ register.php
@@ -3,21 +3,24 @@
 <head>
    
     <title>Register</title>
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="/Mazaj/style/main.css">
     </head>
 <body>
-  <form action="">
+  <form action="register_post.php" method='POST'>
     <div class = "main"> 
       <h2>Register</h2>
       <div class="mb-3">
+        <?php if(isset($user_error)){echo $user_error;}?>
         <label for=""> Username</label>
         <input type= "text" name="username" id="username" placeholder="Enter your Username" required><br>
       </div>
       <div class="mb-3">
+        <?php if(isset($email_error)){echo $email_error;}?>
         <label for=""> Email</label>
         <input type= "text" name="email" id="email" placeholder="Enter your Email" required><br>
       </div>
       <div class="mb-3">
+        <?php if(isset($password_error)){echo $password_error;}?>
         <label for=""> Password</label>
         <input type= "password" name="password" id="password" placeholder="Enter your Password" required><br>
       </div>
